8585f56762df2e715522bfb9998b1296cf7fa797
phuongleeeee <103240378@student.vgu.edu.vn>
2025-06-03 14:22:59 +0700

    Update 📊 Education & Career Success Dashboard.py

--- pages/📊 Education & Career Success Dashboard.py
+++ pages/📊 Education & Career Success Dashboard.py
@@ -14,7 +14,7 @@ def load_data():
 df = load_data()
 
 # === SECTION 1: Career Path Sunburst ===
-with st.expander("🌞 Career Path Sunburst", expanded=True):
+st.markdown("🌞 Career Path Sunburst", expanded=True):
     sunburst_df = df.copy()
 
     def categorize_salary(salary):
